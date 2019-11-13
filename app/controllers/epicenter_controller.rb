5203e2c82b6a7f1bc43caa38f99f8a5754019e15
Tellina <tellinah@gmail.com>
2019-11-12 20:56:59 -0600

    Twitter clone

---
+++ app/controllers/epicenter_controller.rb
@@ -0,0 +1,23 @@
+class EpicenterController < ApplicationController
+  def feed
+  		def feed
+    @following_tweets = []
+
+    Tweet.all.each do |tweet|
+      if current_user.following.include?(tweet.user_id) || current_user.id == tweet.user_id
+        @following_tweets.push(tweet)
+      end
+    end
+end
+  end
+
+def show_user
+  @user = User.find(params[:id])
+end
+
+  def now_following
+  end
+
+  def unfollow
+  end
+end
